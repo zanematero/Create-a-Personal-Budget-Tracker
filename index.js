823a369d13c29e87db3e58598ca5d287e384cdb8
Zane Matero <zanematero@Zanes-MacBook-Pro.local>
2022-10-16 14:05:44 -0700

    Prompting all questions

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+// Weekly Expenses
+for (let i = 0; i < weeklyExpenseQuestions.length; i++)  {
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+    console.log(stringAnswer)
+    let numberAnswer = parseFloat(stringAnswer)
+    weeklyExpenses += numberAnswer
+    console.log(weeklyExpenses)
+}
+// Monthly Expenses
+for (let i = 0; i < monthlyExpenseQuestions.length; i++)  {
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    console.log(stringAnswer)
+    let numberAnswer = parseFloat(stringAnswer)
+    monthlyExpenses += numberAnswer
+    console.log(monthlyExpenses)
+}
+//Annual Expenses
+for (let i = 0; i < annualExpenseQuestions.length; i++)  {
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    console.log(stringAnswer)
+    let numberAnswer = parseFloat(stringAnswer)
+    annualExpenses += numberAnswer
+    console.log(annualExpenses)
+}
